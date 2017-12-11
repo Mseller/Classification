9d23a4ce755c2906ad14916dd2e1a5c9ffc72836
Seller, Martin <martinseller@live.com>
2017-12-11 14:48:00 +0900

    Update

---
+++ source/Classification/models.py
@@ -0,0 +1,157 @@
+import tensorflow as tf
+
+
+__all__ = ['build_model_columns_first', 'build_model_columns_second']
+
+def build_model_columns_first():
+    # Continuous columns
+    age = tf.feature_column.numeric_column('age')
+    education_num = tf.feature_column.numeric_column('education_num')
+    capital_gain = tf.feature_column.numeric_column('capital_gain')
+    capital_loss = tf.feature_column.numeric_column('capital_loss')
+    hours_per_week = tf.feature_column.numeric_column('hours_per_week')
+
+    education = tf.feature_column.categorical_column_with_vocabulary_list(
+        'education', [
+            'Bachelors', 'HS-grad', '11th', 'Masters', '9th', 'Some-college',
+            'Assoc-acdm', 'Assoc-voc', '7th-8th', 'Doctorate', 'Prof-school',
+            '5th-6th', '10th', '1st-4th', 'Preschool', '12th'])
+
+    marital_status = tf.feature_column.categorical_column_with_vocabulary_list(
+        'marital_status', [
+            'Married-civ-spouse', 'Divorced', 'Married-spouse-absent',
+            'Never-married', 'Separated', 'Married-AF-spouse', 'Widowed'])
+
+    relationship = tf.feature_column.categorical_column_with_vocabulary_list(
+        'relationship', [
+            'Husband', 'Not-in-family', 'Wife', 'Own-child', 'Unmarried',
+            'Other-relative'])
+
+    workclass = tf.feature_column.categorical_column_with_vocabulary_list(
+        'workclass', [
+            'Self-emp-not-inc', 'Private', 'State-gov', 'Federal-gov',
+            'Local-gov', '?', 'Self-emp-inc', 'Without-pay', 'Never-worked'])
+
+    # To show an example of hashing:
+    occupation = tf.feature_column.categorical_column_with_hash_bucket(
+        'occupation', hash_bucket_size=1000)
+
+    # Transformations.
+    age_buckets = tf.feature_column.bucketized_column(
+        age, boundaries=[18, 25, 30, 35, 40, 45, 50, 55, 60, 65])
+
+    # Wide columns and deep columns.
+    base_columns = [
+        education, marital_status, relationship, workclass, occupation,
+        age_buckets,
+    ]
+
+    crossed_columns = [
+        tf.feature_column.crossed_column(
+            ['education', 'occupation'], hash_bucket_size=1000),
+        tf.feature_column.crossed_column(
+            [age_buckets, 'education', 'occupation'], hash_bucket_size=1000),
+    ]
+
+    wide_columns = base_columns + crossed_columns
+
+    deep_columns = [
+        age,
+        education_num,
+        capital_gain,
+        capital_loss,
+        hours_per_week,
+        tf.feature_column.indicator_column(workclass),
+        tf.feature_column.indicator_column(education),
+        tf.feature_column.indicator_column(marital_status),
+        tf.feature_column.indicator_column(relationship),
+        # To show an example of embedding
+        tf.feature_column.embedding_column(occupation, dimension=8),
+    ]
+
+    return wide_columns, deep_columns
+
+def build_model_columns_second():
+    age = tf.feature_column.numeric_column('age')
+    education_num = tf.feature_column.numeric_column('education_num')
+    capital_gain = tf.feature_column.numeric_column('capital_gain')
+    capital_loss = tf.feature_column.numeric_column('capital_loss')
+    hours_per_week = tf.feature_column.numeric_column('hours_per_week')
+
+    education = tf.feature_column.categorical_column_with_vocabulary_list(
+        'education', [
+            'Bachelors', 'HS-grad', '11th', 'Masters', '9th', 'Some-college',
+            'Assoc-acdm', 'Assoc-voc', '7th-8th', 'Doctorate', 'Prof-school',
+            '5th-6th', '10th', '1st-4th', 'Preschool', '12th'])
+
+    marital_status = tf.feature_column.categorical_column_with_vocabulary_list(
+        'marital_status', [
+            'Married-civ-spouse', 'Divorced', 'Married-spouse-absent',
+            'Never-married', 'Separated', 'Married-AF-spouse', 'Widowed'])
+
+    relationship = tf.feature_column.categorical_column_with_vocabulary_list(
+        'relationship', [
+            'Husband', 'Not-in-family', 'Wife', 'Own-child', 'Unmarried',
+            'Other-relative'])
+
+    workclass = tf.feature_column.categorical_column_with_vocabulary_list(
+        'workclass', [
+            'Self-emp-not-inc', 'Private', 'State-gov', 'Federal-gov',
+            'Local-gov', '?', 'Self-emp-inc', 'Without-pay', 'Never-worked'])
+
+    native_country = tf.feature_column.categorical_column_with_vocabulary_list(
+        'native_country', [
+            'United-States', 'Cambodia', 'England', 'Puerto-Rico',
+            'Canada', 'Germany', 'Outlying-US(Guam-USVI-etc)', 'India', 'Japan',
+            'Greece', 'South', 'China', 'Cuba', 'Iran', 'Honduras', 'Philippines',
+            'Italy', 'Poland', 'Jamaica', 'Vietnam', 'Mexico', 'Portugal', 'Ireland',
+            'France', 'Dominican-Republic', 'Laos', 'Ecuador', 'Taiwan', 'Haiti',
+            'Columbia', 'Hungary', 'Guatemala', 'Nicaragua', 'Scotland', 'Thailand',
+            'Yugoslavia', 'El-Salvador', 'Trinadad&Tobago', 'Peru', 'Hong', 'Holand-Netherlands'])
+    race = tf.feature_column.categorical_column_with_vocabulary_list(
+        'race', [
+            'White', 'Asian-Pac-Islander', 'Amer-Indian-Eskimo', 'Other', 'Black'])
+
+    # To show an example of hashing:
+    occupation = tf.feature_column.categorical_column_with_hash_bucket(
+        'occupation', hash_bucket_size=1000)
+
+    # Transformations.
+    age_buckets = tf.feature_column.bucketized_column(
+        age, boundaries=[18, 25, 30, 35, 40, 45, 50, 55, 60, 65])
+
+    base_columns = [
+        education, marital_status, relationship, workclass, occupation,
+        age_buckets,
+    ]
+
+    crossed_columns = [
+        tf.feature_column.crossed_column(
+            [age_buckets, 'education'], hash_bucket_size=1000),
+        tf.feature_column.crossed_column(
+            ['native_country', 'race'], hash_bucket_size=1000),
+        tf.feature_column.crossed_column(
+            [age_buckets, 'education', 'occupation'], hash_bucket_size=1000),
+    ]
+
+    base_columns = [
+        education, marital_status, relationship, workclass, occupation,
+        age_buckets, native_country, race,
+    ]
+
+    wide_columns = base_columns + crossed_columns
+
+    deep_columns = [
+        age,
+        education_num,
+        capital_gain,
+        capital_loss,
+        hours_per_week,
+        tf.feature_column.indicator_column(workclass),
+        tf.feature_column.indicator_column(education),
+        tf.feature_column.indicator_column(marital_status),
+        tf.feature_column.indicator_column(relationship),
+        tf.feature_column.indicator_column(native_country),
+        tf.feature_column.indicator_column(occupation),
+    ]
+    return wide_columns, deep_columns
